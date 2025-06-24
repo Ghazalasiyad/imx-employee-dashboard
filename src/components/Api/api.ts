b16184bc21eacbd3b6c379a51b4b36aa741de96b
aliyasmuhammad12 <aliyasmuhammad1122@gmail.com>
2025-06-24 15:02:09 +0500

    changes in the api file

--- src/components/Api/api.ts
+++ src/components/Api/api.ts
@@ -1,25 +1,18 @@
 import axios from 'axios';
-
-const stage = 'prod';
+const stage = 'dev';
 const url = {
-  dev: 'http://localhost:4000/api/v1',
-  prod: 'https://employee.ideometrix.com/api/v1'
+    dev: 'http://localhost:4000/api/v1',
+    prod: 'https://api.ideometrix.com/api/v1'
 };
 const BASE_URL = url[stage];
-
 export const Instance = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true, 
+    baseURL: BASE_URL,
+    withCredentials: true,
 });
-
-
 Instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`; 
-  }
-  return config;
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
-
-
-
